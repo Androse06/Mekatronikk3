a33f2931b50e02085be2586cce193a0a79794a65
Andreas Roset <androse@stud.ntnu.no>
2024-10-15 18:33:38 +0200

    modified:   EngineeringGUi/EngineerDashboardMain.py 	modified:   EngineeringGUi/EngineeringDashboard.ui

--- EngineeringGUi/EngineerDashboardMain.py
+++ EngineeringGUi/EngineerDashboardMain.py
@@ -1,21 +1,53 @@
-# main.py
+### Import for PyQt ###
 from PySide6.QtWidgets import QApplication, QMainWindow
 from EngineeringDashboard import Ui_MainWindow  # Import your generated UI file
 import sys
 
+### Import for ros ###
+import signal
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QFormLayout, QHBoxLayout, QPushButton, QLabel, QSpacerItem, QSizePolicy, QLCDNumber, QDial, QSlider
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QFont
+import rclpy
+from rclpy.node import Node
+from ngc_interfaces.msg import Eta, Nu, Mode
+from ngc_utils.qos_profiles import default_qos_profile
+import numpy as np
+import ngc_utils.math_utils as mu
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+
+
+        ##### ROS MILJØ #####
+
+        # Initialize setpoints
+        self.heading_setpoint = 0.5  # Initial heading setpoint
+        self.surge_setpoint   = 0.0  # Initial surge speed setpoint in knots
+        self.mode_label = 'init'
+
+        # Subscribe to eta_sim and nu_sim
+        self.create_subscription(Eta, 'eta_sim', self.update_eta_feedback, default_qos_profile)
+        self.create_subscription(Nu, 'nu_sim', self.update_nu_feedback, default_qos_profile)
+
+        # Publishers for eta_setpoint and nu_setpoint
+        self.eta_publisher = self.create_publisher(Eta, 'eta_setpoint', default_qos_profile)
+        self.nu_publisher = self.create_publisher(Nu, 'nu_setpoint', default_qos_profile)
+
+        # System mode publisher
+        self.mode_publisher = self.create_publisher(Mode, 'mode', default_qos_profile)
+
+
+
+        ##### PyQt Dashboard #####
 
         # Create an instance of the Ui_MainWindow
         self.ui = Ui_MainWindow()
 
         # Set up the UI
         self.ui.setupUi(self)
-
-        # Initialize variables to hold widget values
-        self.sail_throttle_value = 0
-        self.sail_heading_value = 0
 
         # Connect sliders/dials to methods
         self.ui.Sail_Throttle_Slider.valueChanged.connect(self.update_sail_throttle)
