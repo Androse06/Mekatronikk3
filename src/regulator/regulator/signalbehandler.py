51acf6c170f2cadc9481b31d764d26edb5c0ec91
Andreas Roset <androse@stud.ntnu.no>
2024-10-29 13:32:38 +0100

    modified:   src/regulator/regulator/estimator.py 	modified:   src/regulator/regulator/signalbehandler.py

--- src/regulator/regulator/signalbehandler.py
+++ src/regulator/regulator/signalbehandler.py
@@ -13,9 +13,15 @@ class SignalbehandlingsNode(Node):
     def __init__(self):
         super().__init__('Signalbehandling')
 
+        ### SUB ###
         self.Gnss_Sub       = self.create_subscription(GNSS, 'gnss_measurement', self.gnss_callback, default_qos_profile)
         self.Heading_Sub    = self.create_subscription(HeadingDevice, 'heading_measurement', self.heading_callback, default_qos_profile)
+        
+        ### PUB ###
+        self.Gnss_pub       = self.create_publisher(GNSS, 'gnss_measurement_filtered', default_qos_profile)
+        self.Heading_pub    = self.create_publisher(HeadingDevice, 'heading_measurement_filtered', default_qos_profile)
 
+        ### VARIABLER ###
         self.lat_readings       = np.array([])
         self.lon_readings       = np.array([])
         self.heading_readings   = np.array([])
@@ -26,14 +32,110 @@ class SignalbehandlingsNode(Node):
         self.current_heading    = msg.heading
         self.current_rot        = msg.rot
         self.HeadingState       = msg.valid_signal
-        
+
+        if self.HeadingState:
+            # Rekne ut variansen til målingar
+            self.heading_S, self.heading_average = self.varians_kalkulator(self.heading_readings)
+
+            # Sjekker om ny verdi er innanfor intervall
+            if self.check_intervall(self.current_heading, self.heading_average, self.heading_S, 2):
+                self.heading_readings = np.append(self.heading_readings, self.current_heading)
+                
+                heading_filtered_msg                = HeadingDevice()
+                heading_filtered_msg.heading        = self.current_heading
+                heading_filtered_msg.rot            = self.current_rot
+                heading_filtered_msg.HeadingState   = self.HeadingState
+                self.Heading_pub.publish(heading_filtered_msg)
+
+                if (len(self.heading_readings) > self.max_readings):
+                    self.heading_readings = np.delete(self.heading_readings, 0)
+
+            else:
+                heading_filtered_msg                = HeadingDevice()
+                heading_filtered_msg.HeadingState   = False
+                self.Heading_pub.publish(heading_filtered_msg)
+
+
         
     def gnss_callback(self, msg: GNSS):
         self.current_lat    = msg.lat
         self.current_lon    = msg.lon
+        self.current_sog    = msg.sog
+        self.current_cog    = msg.cog
         self.GnssState      = msg.valid_signal
-        #self.gnss_behandling()
+
+        if self.GnssState:
+            # Rekne ut variansen til målingar
+            self.lat_S, self.lat_average = self.varians_kalkulator(self.lat_readings)
+            self.lon_S, self.lon_average = self.varians_kalkulator(self.lon_readings)
+
+            # Sjekker om nye verdier er innanfor intervall
+            if self.check_intervall(self.current_lat, self.lat_average, self.lat_S, 2):
+                self.lat_readings = np.append(self.lat_readings, self.current_lat)
+                if (len(self.lat_readings) > self.max_readings):
+                    self.lat_readings = np.delete(self.lat_readings, 0)
+                    filter_lat = True
+            else:
+                filter_lat = False
+            
+            if self.check_intervall(self.current_lon, self.lon_average, self.lon_S, 2):
+                self.lon_readings = np.append(self.lon_readings, self.current_lon)
+                if (len(self.lon_readings) > self.max_readings):
+                    self.lon_readings = np.delete(self.lon_readings, 0)
+                    filtered_lon = True
+            else: filtered_lon = False
+
+            if filter_lat and filtered_lon:
+                gnss_filtered_msg               = GNSS()
+                gnss_filtered_msg.lat           = self.current_lat
+                gnss_filtered_msg.lon           = self.current_lon
+                gnss_filtered_msg.sog           = self.current_sog
+                gnss_filtered_msg.cog           = self.current_cog
+                gnss_filtered_msg.valid_signal  = self.GnssState
+                self.Gnss_pub.publish(gnss_filtered_msg)
+
+            else:
+                gnss_filtered_msg               = GNSS()
+                gnss_filtered_msg.valid_signal  = False
+                self.Gnss_pub.publish(gnss_filtered_msg)
+
+                
+            
         
+
+    def varians_kalkulator(self, value):
+        value_average   = 0
+        value_sum       = 0
+        value_emp_var   = 0
+        value_S         = 0
+
+        # Reknar ut gjennomsnitt
+        value_average = sum(value) / len(value)
+
+        # Reknar ut sum for emperisk varians
+        for i in value:
+            value_sum += (i - value_average)**2
+            
+        # Reknar ut empirisk varians
+        value_emp_var = value_sum / (len(value) - 1)
+
+        # Reknar ut empirisk standardavvik
+        value_S = np.sqrt(value_emp_var)
+    
+        return value_S, value_average
+
+    def check_intervall(self, value, average, stand_avvik, toleranse):
+        # Reknar ut intervall etter gitt antall standardavvik
+        nedre_grense = average - (toleranse * stand_avvik)
+        ovre_grense  = average + (toleranse * stand_avvik)
+
+        if (value > nedre_grense) and (value < ovre_grense):
+            return True
+        else:
+            return False
+
+
+    ##### GAMMALT #####
 
     def gnss_behandling(self):
        # Reknar ut gjennomsnitt
