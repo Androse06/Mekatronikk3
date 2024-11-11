194f3a565c792699ab909059216d722ef854bd5b
Andreas Roset <androse@stud.ntnu.no>
2024-11-11 19:21:19 +0100

    modified:   src/ngc_hmi/ngc_hmi/Darkmode_Dashboard.py

--- src/ngc_hmi/ngc_hmi/Darkmode_Dashboard.py
+++ src/ngc_hmi/ngc_hmi/Darkmode_Dashboard.py
@@ -22,11 +22,6 @@ from PySide6.QtWidgets import (QApplication, QDial, QFrame, QGraphicsView,
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide2.QtCore import *
-from PySide2.QtGui import *
-from PySide2.QtWidgets import *
-
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
