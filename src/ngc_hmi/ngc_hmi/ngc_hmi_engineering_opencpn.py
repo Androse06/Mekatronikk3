16de1607a11998a35d7b58cb931f3404ab220a2f
Andreas Roset <androse@stud.ntnu.no>
2024-11-11 20:37:45 +0100

    modified:   src/ngc_hmi/ngc_hmi/ngc_hmi_engineering_opencpn.py

--- src/ngc_hmi/ngc_hmi/ngc_hmi_engineering_opencpn.py
+++ src/ngc_hmi/ngc_hmi/ngc_hmi_engineering_opencpn.py
@@ -399,6 +399,7 @@ class EngineeringHMI(Node):
         hmi_message.eta     = float(mu.mapToPiPi(np.deg2rad(self.eta))) # Convert degrees to radians and map 2 plus minus pi
         hmi_message.anchor  = self.anchor
         self.hmi_publisher.publish(hmi_message)
+        self.get_logger().info(f'anchor = {hmi_message.anchor}')
 
         if self.debug:
             self.get_logger().info(
